d3ab09fa98fc3062b5fd244868b4e6ebca7671c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-04 20:14:24 +0700

    feat: fetch dependents from subsequent pages (#25)
    * test: simplify parse dependents from HTML tests
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: modify `parseDependentsFromHtml` to also parse next page URL
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: modify `fetchDependents` to fetch dependents from subsequent pages
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -6,10 +6,6 @@ it("should parse dependents from HTML data", () => {
       `<!DOCTYPE html>`,
       `<body>`,
       `  <div id="dependents">`,
-      `    <div></div>`,
-      `    <nav></nav>`,
-      `    <details></details>`,
-      `    <p></p>`,
       `    <div>`,
       `      <div></div>`,
       `      <div>`,
@@ -29,56 +25,26 @@ it("should parse dependents from HTML data", () => {
       `        </div>`,
       `      </div>`,
       `    </div>`,
-      `    <div></div>`,
-      `  </div>`,
-      `</body>`,
-    ].join("\n"),
-  );
-
-  expect(dependents).toEqual([
-    { repo: "foo/bar", stars: 11, forks: 13 },
-    { repo: "foo/baz", stars: 13, forks: 17 },
-  ]);
-});
-
-it("should parse dependents from HTML data without details", () => {
-  const dependents = parseDependentsFromHtml(
-    [
-      `<!DOCTYPE html>`,
-      `<body>`,
-      `  <div id="dependents">`,
-      `    <div class="Subhead"></div>`,
-      `    <nav class="tabnav d-flex"></nav>`,
-      `    <p></p>`,
       `    <div>`,
-      `      <div></div>`,
       `      <div>`,
-      `        <img></img>`,
-      `        <span><a>foo</a>/<a>bar</a><small></small></span>`,
-      `        <div>`,
-      `          <span><svg></svg>11</span>`,
-      `          <span><svg></svg>13</span>`,
-      `        </div>`,
-      `      </div>`,
-      `      <div>`,
-      `        <img></img>`,
-      `        <span><a>foo</a>/<a>baz</a><small></small></span>`,
-      `        <div>`,
-      `          <span><svg></svg>13</span>`,
-      `          <span><svg></svg>17</span>`,
-      `        </div>`,
+      `        ::before`,
+      `        <button></button>`,
+      `        <a href="an-url">hello</a>`,
+      `        ::after`,
       `      </div>`,
       `    </div>`,
-      `    <div></div>`,
       `  </div>`,
       `</body>`,
     ].join("\n"),
   );
 
-  expect(dependents).toEqual([
-    { repo: "foo/bar", stars: 11, forks: 13 },
-    { repo: "foo/baz", stars: 13, forks: 17 },
-  ]);
+  expect(dependents).toEqual({
+    dependents: [
+      { repo: "foo/bar", stars: 11, forks: 13 },
+      { repo: "foo/baz", stars: 13, forks: 17 },
+    ],
+    nextPage: "an-url",
+  });
 });
 
 it("should fail to parse dependents from an invalid HTML data", () => {
--- src/parse.ts
+++ src/parse.ts
@@ -10,55 +10,67 @@ export interface Dependent {
  * Parses the dependent repositories from HTML data.
  *
  * @param html - The HTML data.
- * @returns A promise that resolves to a list of dependent repositories.
+ * @returns A promise that resolves to an object containing a list of dependent
+ * repositories and the URL of the next page.
  */
-export function parseDependentsFromHtml(html: string): Dependent[] {
+export function parseDependentsFromHtml(html: string): {
+  dependents: Dependent[];
+  nextPage: string | null;
+} {
   const dom = new JSDOM(html);
   const div = dom.window.document.getElementById("dependents");
-  if (div !== null) {
-    const box = div.children.item(div.children.length - 2);
-    if (box !== null) {
-      const dependents: Dependent[] = [];
+  if (div === null) throw new Error("invalid HTML format");
 
-      for (let i = 1; i < box.children.length; ++i) {
-        const dependent: Dependent = {
-          repo: null,
-          stars: null,
-          forks: null,
-        };
+  const dependents: Dependent[] = [];
+  let nextPage: string | null = null;
 
-        const row = box.children.item(i);
-        if (row !== null) {
-          dependent.repo = "";
-          const span = row.children.item(1);
-          if (span !== null) {
-            const user = span.children.item(0);
-            if (user !== null) dependent.repo += user.textContent;
-            dependent.repo += "/";
-            const repository = span.children.item(1);
-            if (repository !== null) dependent.repo += repository.textContent;
-          }
+  const box = div.children.item(div.children.length - 2);
+  if (box !== null) {
+    for (let i = 1; i < box.children.length; ++i) {
+      const dependent: Dependent = {
+        repo: null,
+        stars: null,
+        forks: null,
+      };
 
-          const div = row.children.item(2);
-          if (div !== null) {
-            const starsSpan = div.children.item(0);
-            if (starsSpan !== null && starsSpan.textContent !== null) {
-              dependent.stars = Number.parseInt(starsSpan.textContent);
-            }
-
-            const forksSpan = div.children.item(1);
-            if (forksSpan !== null && forksSpan.textContent !== null) {
-              dependent.forks = Number.parseInt(forksSpan.textContent);
-            }
-          }
+      const row = box.children.item(i);
+      if (row !== null) {
+        dependent.repo = "";
+        const span = row.children.item(1);
+        if (span !== null) {
+          const user = span.children.item(0);
+          if (user !== null) dependent.repo += user.textContent;
+          dependent.repo += "/";
+          const repository = span.children.item(1);
+          if (repository !== null) dependent.repo += repository.textContent;
         }
 
-        dependents.push(dependent);
+        const div = row.children.item(2);
+        if (div !== null) {
+          const starsSpan = div.children.item(0);
+          if (starsSpan !== null && starsSpan.textContent !== null) {
+            dependent.stars = Number.parseInt(starsSpan.textContent);
+          }
+
+          const forksSpan = div.children.item(1);
+          if (forksSpan !== null && forksSpan.textContent !== null) {
+            dependent.forks = Number.parseInt(forksSpan.textContent);
+          }
+        }
       }
 
-      return dependents;
+      dependents.push(dependent);
     }
   }
 
-  throw new Error("invalid HTML format");
+  const container = div.children.item(div.children.length - 1);
+  if (container !== null) {
+    const div = container.children.item(0);
+    if (div !== null) {
+      const next = div.children.item(1);
+      if (next !== null) nextPage = next.getAttribute("href");
+    }
+  }
+
+  return { dependents, nextPage };
 }
