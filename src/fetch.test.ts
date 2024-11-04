d7f09d2174332d8e19b212b9ff2b4a431248e5d2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-04 20:45:25 +0700

    feat: add a new option to limit fetched dependents
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/fetch.test.ts
+++ src/fetch.test.ts
@@ -2,7 +2,14 @@ import { fetchDependents } from "./fetch.js";
 
 it("should fetch dependents of a repository", async () => {
   const dependents = await fetchDependents("threeal/setup-yarn-action");
-  expect(dependents.length).toBeGreaterThan(0);
+  expect(dependents.length).toBeGreaterThan(30);
+});
+
+it("should fetch some dependents of a repository", async () => {
+  const dependents = await fetchDependents("threeal/setup-yarn-action", {
+    maxFetch: 5,
+  });
+  expect(dependents.length).toBe(5);
 });
 
 it("should fail to fetch dependents of an invalid repository", async () => {
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,13 +1,27 @@
 import { Dependent, parseDependentsFromHtml } from "./parse.js";
 
 /**
+ * Options for fetching dependent repositories.
+ */
+interface FetchDependentsOptions {
+  /**
+   * The maximum number of dependent repositories to fetch.
+   */
+  maxFetch?: number | undefined;
+}
+
+/**
  * Fetches the dependent repositories of a given repository.
  *
  * @param repo - The full name of the repository in the format `user/repository`.
+ * @param options - The options for fetching the dependent repositories.
  * @returns A promise that resolves to a list of dependent repositories.
  * @throws An error if the fetch operation fails.
  */
-export async function fetchDependents(repo: string): Promise<Dependent[]> {
+export async function fetchDependents(
+  repo: string,
+  options?: FetchDependentsOptions,
+): Promise<Dependent[]> {
   const allDependents: Dependent[] = [];
   let url: string | null = `https://github.com/${repo}/network/dependents`;
 
@@ -20,6 +34,15 @@ export async function fetchDependents(repo: string): Promise<Dependent[]> {
     const { dependents, nextPage } = parseDependentsFromHtml(await res.text());
     allDependents.push(...dependents);
     url = nextPage;
+
+    if (
+      options !== undefined &&
+      options.maxFetch !== undefined &&
+      allDependents.length >= options.maxFetch
+    ) {
+      allDependents.splice(options.maxFetch);
+      break;
+    }
   }
 
   return allDependents;
